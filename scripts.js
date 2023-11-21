a1b57a069cc6617627135b38e7f2865455121983
José <helloyinx@gmail.com>
2023-11-21 11:03:33 +0100

    Add delay to animations

--- scripts.js
+++ scripts.js
@@ -1,24 +1,29 @@
 const fadeIn = (el) => {
     let element = document.querySelector(el);
-    element.style.opacity = 1;
-    element.style.marginTop = '-25px';
+    setTimeout(() => {
+        element.style.opacity = 1;
+        element.style.marginTop = '-25px';
+    }, 500); //setTimeout para delay haces función y al cerrarla le das milliseconds    
 };
 
 const myButton = document.querySelector('.cta-start');
 myButton.addEventListener("click", () => {
     let element2 = document.querySelector('.allcontent');
     element2.style.opacity = 0;
-    let element3 = document.querySelector('#flow');
-    element3.style.animationDuration = '0.3';
-    element3.style.width = 0;
+    setTimeout(() => {
+        let element3 = document.querySelector('#flow');
+        element3.style.width = 0;
+    }, 700);
 });
+    
 
 const myButton2 = document.querySelector('.cta-close');
 myButton2.addEventListener('click', () => {
-    let element3 = document.querySelector('#flow');
-    element3.style.width = '100%'; 
-    let element2 = document.querySelector('.allcontent');
-    element3.style.animationDuration = '2s';
-    element2.style.transition = 'all 1s ease-in-out;'
-    element2.style.opacity = 1;
+    let element2 = document.querySelector('#flow');
+    element2.style.width = '100%'; 
+    setTimeout(() => {
+        let element3 = document.querySelector('.allcontent');
+        element3.style.transition = 'all 1s ease-in-out;'
+        element3.style.opacity = 1;
+    }, 1000);
 });
