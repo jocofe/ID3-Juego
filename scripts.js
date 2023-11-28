86e5e4c7891880ad3e3e42c1a9e611c1662b6865
xuani <juasosua@gmail.com>
2023-11-28 20:43:28 +0100

    Nuevos cambios

--- scripts.js
+++ scripts.js
@@ -6,6 +6,27 @@ const fadeIn = (el) => {
     }, 500); //setTimeout para delay haces función y al cerrarla le das milliseconds    
 };
 
+const gameClasification = [
+    {position: '1', name: 'Paquito', points: '50 puntos'},
+    {position: '2', name: 'Marisa', points: '45 puntos'}
+    {position: '3', name: 'Alberto', points: '43 puntos'},
+    {position: '4', name: 'Carlos', points: '40 puntos'}
+    {position: '5', name: 'Laura', points: '37 puntos'},
+    {position: '6', name: 'Juanito', points: '35 puntos'}
+];
+
+const rankingNode = document.querySelector('.ranking');
+
+rankingNode.forEach((positionInfo) => {
+    const pointsLabel = positionInfo.scoring === 1 ? 'punto' : 'puntos';
+
+    rankingHtml += `<div class="ranking__item">
+    <div><strong> ${positionInfo.position}.</strong> ${positionInfo.name}</div>
+    <div>${positionInfo.scoring} ${pointsLabel}</div>
+    </div>`
+    rankingNode.innerHTML = htmlForPosition;
+});
+
 function startGame() {
     const availableClasses = [
         'redsquare--1',
